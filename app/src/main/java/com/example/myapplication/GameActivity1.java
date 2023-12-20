2a38e4fc754aef5ae453dcec71979e9c18ba6dc5
linoy <linoyzilka@gamil.com>
2023-12-20 11:37:38 +0200

    לעשטת פור לערבב שאלות

--- app/src/main/java/com/example/myapplication/GameActivity1.java
+++ app/src/main/java/com/example/myapplication/GameActivity1.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,8 @@ import java.util.ArrayList;
 
 public class GameActivity1 extends AppCompatActivity implements View.OnClickListener{
     private int counter = 0;
+    private int countQ = 0;
+    private QuestionData currentQuestion = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,13 +60,14 @@ public class GameActivity1 extends AppCompatActivity implements View.OnClickList
                             t3.setOnClickListener(GameActivity1.this);
                             t4.setOnClickListener(GameActivity1.this);
 
-                            t.setText(arr.get(0).getQuestion());
-                            ArrayList<String> arr2 = arr.get(0).shuffleQuestions();
+                            t.setText(arr.get(countQ).getQuestion());
+                            ArrayList<String> arr2 = arr.get(countQ).shuffleQuestions();
+                            currentQuestion = arr.get(countQ);
 
-                            //t1.setText(arr.get(0).getA1());
-                            //t2.setText(arr.get(0).getA2());
-                            //t3.setText(arr.get(0).getA3());
-                            //t4.setText(arr.get(0).getA4());
+                            t1.setText(arr2.get(0));
+                            t2.setText(arr2.get(1));
+                            t3.setText(arr2.get(2));
+                            t4.setText(arr2.get(3));
 
 
 
@@ -80,13 +84,15 @@ public class GameActivity1 extends AppCompatActivity implements View.OnClickList
     public void onClick(View v)
     {
         Button b = (Button)v;
-        String s = (String) b.getText();
-        QuestionData t = new QuestionData();
-        if(t.checkAnswer(s))
+        String s = b.getText().toString();
+
+        if(currentQuestion.checkAnswer(s))
         {
             counter++;
-
         }
+        countQ++;
+      //  Intent intent = new Intent(GameActivity1.this, GameActivity1.class);
+       // startActivity(intent);
         getQuestion();
 
     }
--- app/src/main/java/com/example/myapplication/QuestionData.java
+++ app/src/main/java/com/example/myapplication/QuestionData.java
@@ -89,4 +89,5 @@ public class QuestionData
         return arr;
 
     }
+
 }
